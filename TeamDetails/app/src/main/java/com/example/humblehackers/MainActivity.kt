9b674c9417b1425bf2905d7425530effb0fd34b7
SethupathyRajan <rajansethupathyoffl@gmail.com>
2026-01-29 16:08:28 +0530

    feat:initial commit

---
+++ TeamDetails/app/src/main/java/com/example/humblehackers/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.humblehackers
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val suhas = findViewById<ImageView>(R.id.imageView6)
+        val soundar = findViewById<ImageView>(R.id.imageView7)
+        val sethu = findViewById<ImageView>(R.id.imageView8)
+
+        suhas.setOnClickListener{
+            val intent = Intent(this,SuhasActivity::class.java)
+            startActivity(intent)
+        }
+        soundar.setOnClickListener{
+            val intent = Intent(this,SoundarActivity::class.java)
+            startActivity(intent)
+        }
+        sethu.setOnClickListener{
+            val intent = Intent(this,SethuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ TeamDetails/app/src/main/java/com/example/humblehackers/SuhasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.humblehackers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SuhasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.suhasks)
+    }
+
+}
